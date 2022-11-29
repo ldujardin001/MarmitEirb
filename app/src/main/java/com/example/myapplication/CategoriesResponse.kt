22539af0cf55f908a2007f48f57fd8118407b9c0
juliette <jmeurgues@enseirb-matmeca.fr>
2022-11-29 14:47:03 +0100

    First try

---
+++ app/src/main/java/com/example/myapplication/CategoriesResponse.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import com.google.gson.annotations.SerializedName
+
+class CategoriesResponse {
+    var count: Int? = null
+    var previous: String? = null
+    var next: String? = null
+
+    @SerializedName("results")
+    var Categories: List<Category>? = null
+
+}
+
+class Category {
+    var idCategory: String? = null
+    var strCategory: String? = null
+    var strCategporyThumb: String? = null
+    var strCategoryDescription: String? = null
+    /* ... */
+}
